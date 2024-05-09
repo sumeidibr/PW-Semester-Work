f22ec0709ce3121435ab078d4326f993929c9e98
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-05-09 23:46:11 +0200

    Update sobre.php

--- Work/sobre.php
+++ Work/sobre.php
@@ -1,5 +1,22 @@
+    <style>
+        .container-sobre {
+            max-width: 800px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1, p {
+            margin-bottom: 20px;
+        }
+    </style>
 
-<div style="height: 700px; width:100%; background-color:antiquewhite;">
-<h1>Sobre</h1>
-
-</div>
+    <div class="container-sobre">
+        <h1>Sobre Nós</h1>
+        <p>Bem-vindo à nossa empresa de vendas, onde a excelência e o compromisso com a satisfação do cliente são os nossos principais pilares. Desde a nossa fundação, temos nos dedicado a fornecer produtos de qualidade e um serviço excepcional para atender às necessidades dos nossos clientes.</p>
+        <p>Nosso compromisso com a qualidade começa desde a seleção dos produtos que oferecemos em nosso catálogo. Trabalhamos apenas com fornecedores confiáveis e produtos de marcas reconhecidas para garantir que nossos clientes recebam produtos que atendam aos mais altos padrões de qualidade.</p>
+        <p>Além disso, entendemos que o atendimento ao cliente é fundamental para o sucesso de qualquer negócio. É por isso que nossa equipe está sempre disponível para ajudar e responder a quaisquer perguntas ou preocupações que você possa ter. Nosso objetivo é tornar sua experiência de compra o mais fácil e satisfatória possível.</p>
+        <p>Na nossa empresa, valorizamos a transparência e a integridade em todos os aspectos do nosso negócio. Queremos construir relacionamentos sólidos e duradouros com nossos clientes, baseados na confiança e na honestidade.</p>
+        <p>Obrigado por escolher nossa empresa de vendas. Estamos ansiosos para servi-lo e garantir que sua experiência de compra seja excepcional em todos os aspectos.</p>
+    </div>
