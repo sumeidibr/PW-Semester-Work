ec95918f5fded401ecfe5b7f69444e3e73bdb946
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-05-12 13:36:44 +0200

    add

--- Work/tratar_login.php
+++ Work/tratar_login.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location: gestor/index.php");
             }
         }else{
-            echo 'falso';
+            echo 'falsos';
         }
     }
 
