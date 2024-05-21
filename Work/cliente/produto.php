50f2d8f58264adb0ac08804839b5fdc9ea2da5d8
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2024-05-21 22:39:38 +0200

    ultima versao funcional

--- Work/cliente/produto.php
+++ Work/cliente/produto.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
 <body class="body_produto">
 <?php
 include '../gestor.php';
@@ -112,7 +106,7 @@ if (!empty($_POST)) {
   
 </div>
 </body>
-</html>
+
 
 
 
